183e3defe8ea5fc276affa3707ed6d53d1eb67af
beethoven <liujiayi19920724@gmail.com>
2021-01-09 21:14:54 +1300

    Add web view

--- app/src/main/java/com/jiayil/testwebview/MainActivity.kt
+++ app/src/main/java/com/jiayil/testwebview/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.jiayil.testwebview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var browser: WebView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        browser = findViewById<WebView>(R.id.web_view_main)
+        browser?.webViewClient = MyBrowser()
+
+
+        browser?.loadUrl("http://www.google.com")
+    }
+
+    private class MyBrowser : WebViewClient() {
     }
 }
